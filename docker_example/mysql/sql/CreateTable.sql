ea41260144f5f9ce4068bf10928ab0bbfe34e776
qoala <fatchur.rahman1@gmail.com>
2019-07-17 06:31:31 -0400

    ADD mysql docker files

---
+++ docker_example/mysql/sql/CreateTable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE safety.apd (
+id int NOT NULL AUTO_INCREMENT,
+Timestamp_now timestamp NULL,
+Kamera varchar(255) NULL,
+Helm varchar(255) NULL,
+Jacket varchar(255) NULL,
+Kacamata varchar(255) NULL,
+Sarung_tangan varchar(255) NULL,
+Sepatu_safety varchar(255) NULL,
+Lengkap varchar(255) NULL,
+Image_path varchar(255) NULL,
+primary key (id)
+);
---
+++ docker_example/mysql/sql/InsertData.sql
@@ -0,0 +1,2 @@
+INSERT INTO safety.apd (Timestamp_now, Kamera, Helm, Jacket, Lengkap) 
+VALUES ('2019-01-24 11:49:28.220367', 'kamera1', 'OK', 'VIOLATION', 'NO');
